076feeb2ccd0326a49059db0c34b27dbaa8a2275
Ronny Trommer <ronny@opennms.org>
2020-07-03 07:40:51 +0200

    NMS-12771: Fix waiting for Cortex listening ports

--- plugin/src/test/java/org/opennms/timeseries/cortex/CortexTSSIntegrationTest.java
+++ plugin/src/test/java/org/opennms/timeseries/cortex/CortexTSSIntegrationTest.java
@@ -21,9 +21,9 @@ public class CortexTSSIntegrationTest extends AbstractStorageIntegrationTest {
 
     @ClassRule
     public static DockerComposeContainer environment = new DockerComposeContainer<>(new File("src/test/resources/org/opennms/timeseries/cortex/docker-compose.yaml"))
-                .withExposedService("cortex", 9009, 9095, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)))
+                .withExposedService("cortex", 9009, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)))
+                .withExposedService("cortex", 9095, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)))
                 .withExposedService("grafana", 3000, Wait.forListeningPort().withStartupTimeout(Duration.ofSeconds(10)));
-
 
     private CortexTSS cortexTss;
 
